3a652fdc5f5a9be7f95d1604e4cc460db25efad8
Adolf <adolfnyerere@gmail.com>
2023-12-15 18:20:16 +0300

    3 passing

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,20 @@
 #!/usr/bin/env python3
 
-class MyString:
-  pass
+class MyString():
+    def __init__(self, value):
+        if not isinstance(value, str):
+            raise ValueError("The value must be a string.")
+        self.value = value
+
+    def is_sentence(self):
+        return self.value.endswith('.')
+
+    def is_question(self):
+        return self.value.endswith('?')
+
+    def is_exclamation(self):
+        return self.value.endswith('!')
+
+    def count_sentences(self):
+        sentences = [sentence.strip() for sentence in self.value.split('.') if sentence.strip()]
+        return len(sentences)
